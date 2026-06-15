2cb9d976044c89d905f06b90d6c0deb2b42e880f
danieldavid <danieldavidmunoz@yahoo.com>
2026-06-15 17:23:07 +0200

    Actualizacion

---
+++ J01-introduccion/src/introduccion/ejemplos/Ejercicios04/Ej04_04.java
@@ -0,0 +1,19 @@
+//Escriba un programa que imprima los siguientes patrones por separado, uno debajo del siguiente, 
+//una vez que el usuario ingresó el tamaño, por ejemplo 5. Utilice ciclos for para generar los patrones. 
+//Todos los asteriscos deberán ser impresos por un solo enunciado print de la forma print(‘*’). 
+//Sugerencia: los dos últimos patrones requieren que cada línea comience con un número correcto de espacios en blanco: print(‘ ’).
+//* ***** ***** *
+//** **** **** **
+//*** *** *** ***
+//**** ** ** ****
+//***** * * *****
+package introduccion.ejemplos.Ejercicios04;
+import introduccion.ejemplos.Ejercicios05.Consola;
+public class Ej04_04 {
+
+	public static void main (String arg[]) {
+		                                             
+	}
+	
+	
+}
