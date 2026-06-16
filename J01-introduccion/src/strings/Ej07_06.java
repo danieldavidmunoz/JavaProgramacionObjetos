b341d57e0e9ecf8f23eac4d37596b93616d7943a
danieldavid <danieldavidmunoz@yahoo.com>
2026-06-16 19:22:35 +0200

    Actualizado

--- J01-introduccion/src/strings/Ej07_06.java
+++ J01-introduccion/src/strings/Ej07_06.java
@@ -1,5 +1,18 @@
+//Método que cuente el número de veces que aparece un carácter en una cadena.
+
 package strings;
 
 public class Ej07_06 {
+	public static int cuentaCaracteres(String s) {
+		int contador=0;
+		while (contador<s.length())
+		contador++; 			
+		return contador;
+		}
+	public static void main (String [] args) {
+		System.out.println(cuentaCaracteres("hola"));
+		System.out.println(cuentaCaracteres("terremotos"));
+		System.out.println(cuentaCaracteres("cochecito"));
+	}
 
 }
