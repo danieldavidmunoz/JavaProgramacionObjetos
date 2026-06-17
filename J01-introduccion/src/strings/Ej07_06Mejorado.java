fc7113b0f4783e50a6a90afadd6e101bd253f6d7
danieldavidmunoz <danieldavidmunoz@yahoo.com>
2026-06-17 13:59:21 +0200

    Mejorado-> Mejora

--- J01-introduccion/src/strings/Ej07_06Mejorado.java
+++ J01-introduccion/src/strings/Ej07_06Mejorado.java
@@ -1,5 +1,19 @@
 package strings;
-
+// Método que cuente el número de veces que aparece un carácter en una cadena.
 public class Ej07_06Mejorado {
+     public static int cuentaCaracter (String cadena,char caracter) {   
+    int veces=0;
+    for (int i=0;i<cadena.length(); i++) {
 
+        if (cadena.charAt(i)==caracter) veces++;
+    } 
+    return veces;
 }
+    public static void main (String [] args) {
+        System.out.println(cuentaCaracter("casa", 'a'));
+        System.out.println(cuentaCaracter("coche de calle", 'a'));
+		}
+    }
+
+
+
