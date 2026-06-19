cd495e2fe43758439d1c5a178a325299bc584384
danieldavid <danieldavidmunoz@yahoo.com>
2026-06-19 15:58:24 +0200

    avanzamdo

---
+++ J01-introduccion/src/arrays/Array02_profe.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class Array02_profe {
+    public static void main (String [] arg){
+    int v1,v2;
+    if(arg.length !=2){
+        System.out.println("dame enteros");
+        return;
+    }
+    v1=Integer.parseInt(arg[0]);
+    v1=Integer.parseInt(arg[1]);
+    }
+}
